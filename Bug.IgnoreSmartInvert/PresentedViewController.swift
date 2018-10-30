a64d34b18d62ac0a9aaa98e592cdab1c56f14689
Ethan Huang <blesserx@gmail.com>
2018-10-30 16:50:55 +0800

    - Add README - Add UIView.appearance().accessibilityIgnoresInvertColors = true to application(didFinishLaunchingWithOptions:) - Add presented view controller

---
+++ Bug.IgnoreSmartInvert/PresentedViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PresentedViewController.swift
+//  Bug.IgnoreSmartInvert
+//
+//  Created by Ethanhuang on 2018/10/30.
+//  Copyright © 2018年 Elaborapp Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+class PresentedViewController: UIViewController {
+    @IBAction func dismiss() {
+        dismiss(animated: true)
+    }
+}
