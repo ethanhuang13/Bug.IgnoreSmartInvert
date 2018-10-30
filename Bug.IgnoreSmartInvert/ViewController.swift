83b8d1dd2b580bb02f216a122a74c1f1f6a886f7
Ethan Huang <blesserx@gmail.com>
2018-10-30 16:17:42 +0800

    Initial Commit

---
+++ Bug.IgnoreSmartInvert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bug.IgnoreSmartInvert
+//
+//  Created by Ethanhuang on 2018/10/30.
+//  Copyright © 2018年 Elaborapp Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
